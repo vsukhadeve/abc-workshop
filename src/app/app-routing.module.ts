eca279c6d122798c6bd61982a27ee24611ca105b
vijay sukhadeve <vihang1301@gmail.com>
2020-09-02 10:36:34 -0700

    "Initial Commit"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '',redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m=>m.DashboardModule)}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
